e39339cabb76d363783e0c908532ea7c89464282
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2026-04-14 10:47:02 +0200

    feat: deps update

--- index.js
+++ index.js
@@ -8,21 +8,21 @@ import { dataset, dispatchCustomEvent } from '@newlogic-digital/utils-js'
  * @returns void
  */
 export const initNaja = (element, bindUI = true, selectors = 'button, [role="button"]') => {
-    bindUI && naja.uiHandler.bindUI(element)
+  bindUI && naja.uiHandler.bindUI(element)
 
-    element.querySelectorAll(`:where(${selectors})${naja.uiHandler.selector}`).forEach((element) => {
-        if (element.form && element.type === 'submit') return
+  element.querySelectorAll(`:where(${selectors})${naja.uiHandler.selector}`).forEach((element) => {
+    if (element.form && element.type === 'submit') return
 
-        element.addEventListener('click', async function interaction(event) {
-            await naja.uiHandler.processInteraction(element, element.dataset.najaMethod ?? 'GET', element.dataset.najaUrl, element.dataset.najaData, {}, event)
+    element.addEventListener('click', async function interaction(event) {
+      await naja.uiHandler.processInteraction(element, element.dataset.najaMethod ?? 'GET', element.dataset.najaUrl, element.dataset.najaData, {}, event)
 
-            if (element.dataset.naja?.includes('once')) {
-                element.removeAttribute('data-naja')
+      if (element.dataset.naja?.includes('once')) {
+        element.removeAttribute('data-naja')
 
-                element.removeEventListener('click', interaction)
-            }
-        })
+        element.removeEventListener('click', interaction)
+      }
     })
+  })
 }
 
 /**
@@ -30,87 +30,87 @@ export const initNaja = (element, bindUI = true, selectors = 'button, [role="but
  * @returns import('naja/dist/Naja').Extension
  */
 export const NajaCoreExtension = (options = {}) => {
-    return {
-        initialize(naja) {
-            naja.uiHandler.selector = options.selector ?? '[data-naja]'
+  return {
+    initialize(naja) {
+      naja.uiHandler.selector = options.selector ?? '[data-naja]'
 
-            initNaja(document.body, false, options.selectors)
+      initNaja(document.body, false, options.selectors)
 
-            naja.uiHandler.addEventListener('interaction', (event) => {
-                dispatchCustomEvent(event.detail.element, 'naja:interaction', {
-                    detail: event.detail
-                })
+      naja.uiHandler.addEventListener('interaction', (event) => {
+        dispatchCustomEvent(event.detail.element, 'naja:interaction', {
+          detail: event.detail,
+        })
 
-                event.detail.options.interactionElement = event.detail.element
+        event.detail.options.interactionElement = event.detail.element
 
-                const form = event.detail?.originalEvent?.target
+        const form = event.detail?.originalEvent?.target
 
-                if (form?.gtoken && (!event.detail?.originalEvent?.detail?.recaptchaExecuted || !form?.checkValidity())) {
-                    event.preventDefault()
-                }
-            })
-
-            naja.snippetHandler.addEventListener('afterUpdate', (event) => {
-                dispatchCustomEvent(event.detail.snippet, 'naja:afterUpdate', {
-                    detail: event.detail
-                })
-
-                initNaja(event.detail.snippet, true, options.selectors)
-            })
-
-            naja.historyHandler.addEventListener('buildState', ({ detail }) => {
-                detail.state.source = options.popstateSource ?? 'swup'
-            })
-
-            naja.addEventListener('success', (event) => {
-                if (event.detail?.payload?.formId && event?.detail?.payload?.formStatus) {
-                    dispatchCustomEvent(document.getElementById(event.detail.payload.formId), `naja:form-${event.detail.payload.formStatus}`, {
-                        detail: event.detail
-                    })
-                }
-            })
-
-            naja.addEventListener('start', (event) => {
-                event.detail.options.interactionElement?.setAttribute(options.loadingAttribute ?? 'data-loading', '')
-            })
-
-            naja.addEventListener('complete', (event) => {
-                const elements = [
-                    event.detail.options.interactionElement,
-                    event.detail.options.interactionElement?.querySelector(`button[type="submit"]`)
-                ]
-
-                elements.forEach(element => element?.removeAttribute(options.loadingAttribute ?? 'data-loading'))
-            })
+        if (form?.gtoken && (!event.detail?.originalEvent?.detail?.recaptchaExecuted || !form?.checkValidity())) {
+          event.preventDefault()
         }
-    }
+      })
+
+      naja.snippetHandler.addEventListener('afterUpdate', (event) => {
+        dispatchCustomEvent(event.detail.snippet, 'naja:afterUpdate', {
+          detail: event.detail,
+        })
+
+        initNaja(event.detail.snippet, true, options.selectors)
+      })
+
+      naja.historyHandler.addEventListener('buildState', ({ detail }) => {
+        detail.state.source = options.popstateSource ?? 'swup'
+      })
+
+      naja.addEventListener('success', (event) => {
+        if (event.detail?.payload?.formId && event?.detail?.payload?.formStatus) {
+          dispatchCustomEvent(document.getElementById(event.detail.payload.formId), `naja:form-${event.detail.payload.formStatus}`, {
+            detail: event.detail,
+          })
+        }
+      })
+
+      naja.addEventListener('start', (event) => {
+        event.detail.options.interactionElement?.setAttribute(options.loadingAttribute ?? 'data-loading', '')
+      })
+
+      naja.addEventListener('complete', (event) => {
+        const elements = [
+          event.detail.options.interactionElement,
+          event.detail.options.interactionElement?.querySelector(`button[type="submit"]`),
+        ]
+
+        elements.forEach(element => element?.removeAttribute(options.loadingAttribute ?? 'data-loading'))
+      })
+    },
+  }
 }
 
 export const NajaInvokeExtension = () => {
-    return {
-        initialize(naja) {
-            naja.snippetHandler.addEventListener('afterUpdate', ({ detail }) => {
-                const interactionElement = detail.options.interactionElement
+  return {
+    initialize(naja) {
+      naja.snippetHandler.addEventListener('afterUpdate', ({ detail }) => {
+        const interactionElement = detail.options.interactionElement
 
-                if (!interactionElement || !interactionElement?.getAttribute('data-naja')?.includes('invoke')) return
+        if (!interactionElement || !interactionElement?.getAttribute('data-naja')?.includes('invoke')) return
 
-                interactionElement.setAttribute('data-invoke-target', `#${detail.snippet.id}`)
-                dataset(interactionElement, 'action').add('invoke#action')
-            })
-        }
-    }
+        interactionElement.setAttribute('data-invoke-target', `#${detail.snippet.id}`)
+        dataset(interactionElement, 'action').add('invoke#action')
+      })
+    },
+  }
 }
 
 export const NajaCheckValidityExtension = () => {
-    return {
-        initialize(naja) {
-            naja.uiHandler.addEventListener('interaction', (event) => {
-                const { element } = event.detail
+  return {
+    initialize(naja) {
+      naja.uiHandler.addEventListener('interaction', (event) => {
+        const { element } = event.detail
 
-                if (element?.form && !element.form.checkValidity()) {
-                    event.preventDefault()
-                }
-            })
+        if (element?.form && !element.form.checkValidity()) {
+          event.preventDefault()
         }
-    }
+      })
+    },
+  }
 }
